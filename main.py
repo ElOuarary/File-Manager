07a83bc16a315789f05577baa43c4d55cda1db63
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2025-03-31 19:16:35 +0000

    Modify the documentation of the main file and add the functionality to input the folder to organize

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import os
 from shutil import move
+import sys
 
-source_folder = 'C:\\Users\\dell\\Downloads'
 
-# Set the category for different files type
+# Get the source folder absolute path and check for it existence
+source_folder = input('Enter the absoltue path for the folder to organize: ')
+if not os.path.exists(source_folder):
+    print(f'{source_folder} does not exists in your device.')
+    sys.exit()
+
+
+# Set the category for the different files type
 file_types = {
     'Text': ['.txt', '.rtf', '.docx', '.csv', '.doc', '.wps', '.wpd', '.msg', '.pdf'],
     'Image': ['.jpg', '.png', '.webp', '.gif', '.tif', '.bmp', '.eps'],
@@ -14,15 +21,23 @@ file_types = {
     'Web Page': ['.html', 'htm', '.xhtml', '.asp', '.css', '.aspx', '.rss']
 }
 
+
 # Create the categorical folders
 for category in file_types.keys():
     folder_path = os.path.join(source_folder, category)
+    if os.path.exists(folder_path):
+        answer = input(f'{category} alrady exists do you want to overwrite it [Y/N]: ').lower()
+        if answer == 'n':
+            continue
+        else:
+            os.removedirs(folder_path)
     os.makedirs(folder_path, exist_ok=True)
     
 
-# Manage the files
+# Get the list of files and folder in the source folder
 ls_dir = os.listdir(source_folder)
 
+# Organize the files
 for element in ls_dir:
     element_path = os.path.join(source_folder, element)
     
@@ -33,7 +48,7 @@ for element in ls_dir:
     
     category = ''
     for cat, extensions in file_types.items():
-        if extension in extensions:
+        if extension.lower() in extensions:
             category = cat
             break
     
