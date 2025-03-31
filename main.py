cd6decc919b85c7bacb4809399588125d8aa3161
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2025-03-31 19:30:39 +0000

    Created a branch for handling more complex inputs

--- main.py
+++ main.py
@@ -3,8 +3,24 @@ from shutil import move
 import sys
 
 
+# Check for the validity of the folder
+def check_folder(folder_path: str) -> None:
+    if not os.path.exists(folder_path):
+        print(f'{folder_path} does not exists!')
+        sys.exit()
+    if not os.path.isdir(folder_path):
+        print(f'{folder_path} is not a folder!')
+        sys.exit()
+
+
 # Get the source folder absolute path and check for it existence
-source_folder = input('Enter the absoltue path for the folder to organize: ')
+def set_parameters() -> tuple[str, str]:
+    source_folder = input('Enter the absolute path for the source folder: ')
+    destination_folder = input('Enter the absolute path for the destination folder: ')
+    return (source_folder, destination_folder)
+
+
+source_folder, destination_folder = set_parameters()
 if not os.path.exists(source_folder):
     print(f'{source_folder} does not exists in your device.')
     sys.exit()
@@ -52,5 +68,5 @@ for element in ls_dir:
             category = cat
             break
     
-    destination_path = os.path.join(source_folder, category, element)
+    destination_path = os.path.join(destination_folder, category, element)
     move(element_path, destination_path)
