ff4202f95bb74fbdb5ce2f3c3d6a5f378ea84d7f
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2025-04-03 23:39:40 +0000

    Create the organize function, the rest is to test it

--- main.py
+++ main.py
@@ -33,26 +33,25 @@ def create_folders(destination_path: str) -> None:
         os.makedirs(folder_path)
 
 
-source_folder, destination_folder = set_parameters()
-    
-
-# Get the list of files and folder in the source folder
-ls_dir = os.listdir(source_folder)
-
 # Organize the files
-for element in ls_dir:
-    element_path = os.path.join(source_folder, element)
-    
-    if os.path.isdir(element):
-        continue
-    
-    _, extension = os.path.splitext(element_path)
-    
-    category = ''
-    for cat, extensions in file_types.items():
-        if extension.lower() in extensions:
-            category = cat
-            break
-    
-    destination_path = os.path.join(destination_folder, category, element)
-    move(element_path, destination_path)
+def organize(source_folder: str, destination_folder: str) -> None:
+    source_dir = os.listdir(source_folder)
+    for element in source_dir:
+        element_path = os.path.join(source_folder, element)
+        
+        if os.path.isdir(element):
+            continue
+        
+        _, extension = os.path.splitext(element_path)
+        category = ''
+        for cat, extensions in file_types.items():
+            if extension.lower() in extensions:
+                category = cat
+                break
+        
+        destination_to_move = os.path.join(destination_folder, category, element)
+        move(element_path, destination_to_move)
+        print(f'{element} move from {element_path} -> {os.path.join(destination_folder, category)}')
+        
+
+source_folder, destination_folder = set_parameters()
