34d5875f87b58df62099a4aeae9995ddfc64a287
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2025-04-03 23:28:24 +0000

    Create the functions that will handle the different part of the programme

---
+++ file_types.py
@@ -0,0 +1,10 @@
+# Set the category for the different files type
+file_types = {
+    'Text': ['.txt', '.rtf', '.docx', '.csv', '.doc', '.wps', '.wpd', '.msg', '.pdf'],
+    'Image': ['.jpg', '.png', '.webp', '.gif', '.tif', '.bmp', '.eps'],
+    'Audio': ['.mp3', '.wma', '.snd', '.snd', '.wav', '.ra', '.au', '.aac'],
+    'Video': ['.mp4', '3gp', '.avi', '.mpg', '.mov', '.wmv'],
+    'Program': ['.c', '.cpp', '.java', '.py', '.js', '.ts', '.cs', '.swift', '.dta', '.pl', '.sh', '.bat', '.com', '.exe'],
+    'Comressed': ['.rar', '.zip', '.hqx', '.arj', '.tar', '.arc', '.sit', '.gz', '.z'],
+    'Web Page': ['.html', 'htm', '.xhtml', '.asp', '.css', '.aspx', '.rss']
+}
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from shutil import move
 import sys
 
+from file_types import file_types
 
 # Check for the validity of the folder
 def check_folder(folder_path: str) -> None:
@@ -16,38 +17,23 @@ def check_folder(folder_path: str) -> None:
 # Get the source folder absolute path and check for it existence
 def set_parameters() -> tuple[str, str]:
     source_folder = input('Enter the absolute path for the source folder: ')
+    check_folder(source_folder)
     destination_folder = input('Enter the absolute path for the destination folder: ')
+    check_folder(destination_folder)
     return (source_folder, destination_folder)
 
 
-source_folder, destination_folder = set_parameters()
-if not os.path.exists(source_folder):
-    print(f'{source_folder} does not exists in your device.')
-    sys.exit()
-
-
-# Set the category for the different files type
-file_types = {
-    'Text': ['.txt', '.rtf', '.docx', '.csv', '.doc', '.wps', '.wpd', '.msg', '.pdf'],
-    'Image': ['.jpg', '.png', '.webp', '.gif', '.tif', '.bmp', '.eps'],
-    'Audio': ['.mp3', '.wma', '.snd', '.snd', '.wav', '.ra', '.au', '.aac'],
-    'Video': ['.mp4', '3gp', '.avi', '.mpg', '.mov', '.wmv'],
-    'Program': ['.c', '.cpp', '.java', '.py', '.js', '.ts', '.cs', '.swift', '.dta', '.pl', '.sh', '.bat', '.com', '.exe'],
-    'Comressed': ['.rar', '.zip', '.hqx', '.arj', '.tar', '.arc', '.sit', '.gz', '.z'],
-    'Web Page': ['.html', 'htm', '.xhtml', '.asp', '.css', '.aspx', '.rss']
-}
-
-
 # Create the categorical folders
-for category in file_types.keys():
-    folder_path = os.path.join(source_folder, category)
-    if os.path.exists(folder_path):
-        answer = input(f'{category} alrady exists do you want to overwrite it [Y/N]: ').lower()
-        if answer == 'n':
+def create_folders(destination_path: str) -> None:
+    for category in file_types.keys():
+        folder_path = os.path.join(destination_path, category)
+        if os.path.exists(folder_path):
+            print(f'{category} folder already exists!')
             continue
-        else:
-            os.removedirs(folder_path)
-    os.makedirs(folder_path, exist_ok=True)
+        os.makedirs(folder_path)
+
+
+source_folder, destination_folder = set_parameters()
     
 
 # Get the list of files and folder in the source folder
