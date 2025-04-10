3a982afb1f0969ef1d3e5164799b6a5ec60f068b
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2025-04-10 18:46:13 +0100

    This is for today

--- main.py
+++ main.py
@@ -14,19 +14,24 @@ def check_folder(folder_path: str) -> None:
         sys.exit()
 
 
-# Get the source folder absolute path and check for it existence
-def set_parameters() -> tuple[str, str]:
-    source_folder = input('Enter the absolute path for the source folder: ')
-    check_folder(source_folder)
-    destination_folder = input('Enter the absolute path for the destination folder: ')
-    check_folder(destination_folder)
-    return (source_folder, destination_folder)
+# Get the absolute path and check for it
+def get_path():
+    folder_path = input('Enter the absolute path: ')
+    check_folder(folder_path)
+    return folder_path
+
+# Check the responce
+def check_responce(responce):
+    while True:
+        if responce.upper() not in ['Y', 'N']:
+            responce = input('Your respnce should be either "Y" or "N", try again: ')
+        return responce
 
 
 # Create the categorical folders
-def create_folders(destination_path: str) -> None:
+def create_folders(folder_path: str) -> None:
     for category in file_types.keys():
-        folder_path = os.path.join(destination_path, category)
+        folder_path = os.path.join(folder_path, category)
         if os.path.exists(folder_path):
             print(f'{category} folder already exists!')
             continue
@@ -52,6 +57,3 @@ def organize(source_folder: str, destination_folder: str) -> None:
         destination_to_move = os.path.join(destination_folder, category, element)
         move(element_path, destination_to_move)
         print(f'{element} move from {element_path} -> {os.path.join(destination_folder, category)}')
-        
-
-source_folder, destination_folder = set_parameters()
